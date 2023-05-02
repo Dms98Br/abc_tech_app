87f921e08d0e18a818d9623f2ad97d42b54a32d4
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-05-01 22:52:06 -0300

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const urlProd =
+      "abactechapi-env.eba-kthzgquw.us-east-1.elasticbeanstalk.com";
+  static const urlDev = "http://localhost";
+  static const url = urlDev;
+}
